e807637e3283f26da1c5fdffc9488b734ff89c25
Jhustine Torres <coreihard1@gmail.com>
2023-07-25 02:01:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mvvmtodopapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mvvmtodopapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvmtodopapp.ui.theme.MVVMTodopAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MVVMTodopAppTheme {
+
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MVVMTodopAppTheme {
+
+    }
+}
---
+++ app/src/main/java/com/example/mvvmtodopapp/data/TodoDao.kt
@@ -0,0 +1,24 @@
+package com.example.mvvmtodopapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao { // Dao means "Data access object" witch define diff ways to access our databse
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // replace the old one if the id is same so that we can have insert and update action just in one function
+    suspend fun insertTodo(todo: Todo) //suspend fun suspend the activity until get a results
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+
+    @Query("SELECT * FROM todo WHERE id = :id")// use colon ":" like :id to refer in parameter
+    suspend fun getTodoById(id: Todo): Todo? // return null if did not exist to not crash the app
+
+    @Query("SELECT * FROM todo")
+    fun getTodos(): Flow<List<Todo>>
+}
